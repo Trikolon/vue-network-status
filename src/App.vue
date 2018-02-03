625bb0eca703960eb3b907d0f86783bb00c27b5b
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-02-03 02:30:04 +0100

    Changed description

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
     <h1>Network Status</h1>
-    <p>These metrics are reported by your browser and therefore might be inaccurate.</p>
+    <p>
+      These metrics are reported by your browser and therefore might be inaccurate.
+      Try
+      <a href="https://play.google.com/store/apps/details?id=com.android.chrome" rel="noopener" target="_blank">
+        Google Chrome on Android
+      </a> for the best results.</p>
     <p>Data is updated automatically.</p>
     <NetworkStatus/>
   </div>
 </template>
 
 <script>
-import NetworkStatus from './components/NetworkStatus';
+  import NetworkStatus from './components/NetworkStatus';
 
-export default {
-  name: 'app',
-  components: {
-    NetworkStatus,
-  },
-};
+  export default {
+    name: 'app',
+    components: {
+      NetworkStatus,
+    },
+  };
 </script>
 
 <style>
-#app {
-@import url('https://fonts.googleapis.com/css?family=Roboto');
-  font-family: 'Roboto', sans-serif;
-  text-align: center;
-  margin: 5em;
-}
+  #app {
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    margin: 5em;
+  }
 </style>
